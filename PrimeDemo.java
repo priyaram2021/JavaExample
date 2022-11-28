7a480b951daf58bd9050adae46f499e04fa1a86e
SARAVANA KUMAR N <priyaram2021@gmail.com>
2022-11-28 17:36:34 +0530

    updated

---
+++ PrimeDemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class PrimeDemo
+{
+  public static void main(String args[])
+  {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int prime = input.nextInt();
+	int digit;
+	int count=0;
+		for(int i=1; i<= prime; i++)
+		{
+			if(prime%i==0)
+			{
+				
+				count++;
+			}
+			
+		}
+		if(count==2)
+		{
+			System.out.println("Its  a prime number");	
+		}
+		else
+		{
+			System.out.println("not  a prime number");	
+		}
+  }
+}
+ 
